3635cd01ae7f92de678987fbca59a6340a7da033
raviravick <raviravick63@gmail.com>
2025-07-26 11:19:56 +0530

    Add files via upload

---
+++ array creation.py
@@ -0,0 +1,7 @@
+import numpy as np
+arr=np.array([[1,2,3],[4,2,5]])
+print("array is of type:",type(arr))
+print("number of dimensions:",arr.ndim)
+print("shape of array:",arr.shape)
+print("size of array:",arr.size)
+print("array stores element type:",arr.dtype)
